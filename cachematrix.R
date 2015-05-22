feee0305927f36343ecc3ee1f0afbd8ae0ea7c3a
RomanLad <rladynskyi@gmail.com>
2015-05-22 09:59:56 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,35 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Function sets the value of the matrix and stores inversed matrix in cache
 
 makeCacheMatrix <- function(x = matrix()) {
-        m <- NULL
-        set <- function(y) {
-                x <<- y
-                m <<- NULL
-        }
-        get <- function() x
-        setinverse <- function(invesrse) m <<- invesrse
-        getinverse <- function() m
-        list(set = set, get = get,
+        m <- NULL ## m is NULL 
+        set <- function(y) { ## Set function created, which sets the value of the matrix 
+                x <<- y ## getting the value from the cash and sets the value 
+                m <<- NULL ##if matrix exists sets value to NULL
+        }## list of functions to put matrix in cache
+        get <- function() x ## get the value of the matrix
+        setinverse <- function(invesrse) m <<- invesrse ## stores inversed value of the matrix
+        getinverse <- function() m ## store inversed matrix
+        list(set = set, get = get, ## list of functions to be returned to the above 
              setinverse = setinverse,
              getinverse = getinverse)
 }
 
-## Write a short comment describing this function
+## Function returns and computes inversed matrix from cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-        m <- x$getinverse()
-        if(!is.null(m)) {
-                message("getting cached data")
-                return(m)
+        m <- x$getinverse()## get the matrix if it is stored in cache
+        if(!is.null(m)) {## if inverse matrix stored proceed to cache
+                message("getting cached data")## prints message
+                return(m)#return the inversed matrix
         }
-        data <- x$get()
-        m <- solve(data, ...)
-        x$setinverse(m)
-        m
+        data <- x$get()## if not in cache, solves the matrix
+        m <- solve(data, ...)## matrix inversed
+        x$setinverse(m)## inversed matrix to cache 
+        m ## casched matrix is printed
 }
 
 
